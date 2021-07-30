a03c82119b996a1abb43883aa5c987def006f986
David Svensson <david.svensson@orosberg.com>
2021-07-30 11:00:53 +0200

    Fixed invite functionality that was causing some issues

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
 
   has_many :employments, dependent: :destroy
 
+  validates_uniqueness_of :email, case_sensitive: false, allow_blank: false
+  validates :email, presence: true
   validates :password, length: { minimum: 6 }
 
   def manages_organizations
